f46ea122534d244c754a6f9a78ac0b0ab29c527b
Buster Benson <busterbenson@users.noreply.github.com>
2021-09-14 19:54:20 -0700

    three cards from three decks

---
+++ app.js
@@ -0,0 +1,46 @@
+var createError  = require('http-errors');
+var express      = require('express');
+var path         = require('path');
+var cookieParser = require('cookie-parser');
+var logger       = require('morgan');
+
+var indexRouter  = require('./routes/index');
+var usersRouter  = require('./routes/users');
+
+var app          = express();
+
+// view engine setup
+// learn more: https://pugjs.org/api/reference.html
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
+app.use("/stylesheets", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); 
+
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router  = express.Router();
+
+// custom modules
+var fs      = require('fs');
+var yaml    = require('js-yaml');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var params = {
+    title: 'Brackish Cards',
+    tarot: null,
+    cards: [],
+    card_tokens: []
+  }
+
+  try {
+    // Load 3 tarot cards
+    params['tarot'] = yaml.load(fs.readFileSync('./data/tarot.yaml', 'utf8'));
+    var all_cards = Object.keys(params['tarot'])
+    while (params['cards'].length < 3) {
+      var random_index = Math.floor(Math.random() * 78)
+      var card = params['tarot'][all_cards[random_index-1]]
+      if (!params['cards'].includes(card)) {
+        params['cards'].push(card)
+        params['card_tokens'].push(all_cards[random_index-1])
+        console.log(all_cards[random_index-1])
+      }
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  res.render('index', params);
+});
+
+module.exports = router;
