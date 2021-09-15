819c0e7700a01abe08c3a83f1156bd07753411ec
Buster Benson <busterbenson@users.noreply.github.com>
2021-09-15 01:27:43 -0700

    reload cards to answer questions

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,27 @@ router.get('/', function(req, res, next) {
     title: 'Brackish Cards',
     tarot: null,
     cards: [],
-    card_tokens: []
+    card_tokens: [],
+    card_index: [],
+    deck_tokens: ['rws', 'thoth', 'cbd', 'mary', 'brady']
   }
 
   try {
-    // Load 3 tarot cards
+    // Load all the cards across all decks
     params['tarot'] = yaml.load(fs.readFileSync('./data/tarot.yaml', 'utf8'));
+    // Load all the different decks and metadata about them
     params['decks'] = yaml.load(fs.readFileSync('./data/decks.yaml', 'utf8'));
-    var all_cards = Object.keys(params['tarot'])
+    // All of the card tokens
+    params['all_cards'] = Object.keys(params['tarot'])
+
+    // Get 3 cards to display initially
     while (params['cards'].length < 3) {
       var random_index = Math.floor(Math.random() * 78)
-      var card = params['tarot'][all_cards[random_index-1]]
+      var card = params['tarot'][params['all_cards'][random_index-1]]
       if (!params['cards'].includes(card)) {
         params['cards'].push(card)
-        params['card_tokens'].push(all_cards[random_index-1])
+        params['card_tokens'].push(params['all_cards'][random_index-1])
+        params['card_index'].push(random_index-1)
       }
     }
   } catch (e) {
