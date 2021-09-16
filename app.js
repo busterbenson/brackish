3a840c2e4b19541891046bd2740c3f7c98d11a79
Buster Benson <busterbenson@users.noreply.github.com>
2021-09-15 21:38:27 -0700

    a way to do prompts

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
 app.use("/stylesheets", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); 
 
-// Routes
-app.use('/', indexRouter);
+// Routers
+app.use(indexRouter);
 app.use(readingRouter);
 
 // Catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,22 @@ var fs      = require('fs');
 var yaml    = require('js-yaml');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-
+router.get(['/','/spread/:spread'], function(req, res, next) {
   var params = {
     title: 'Brackish Cards',
     tarot: null,
     cards: [],
     card_tokens: [],
     card_index: [],
-    deck_tokens: ['rws', 'thoth', 'cbd', 'mary', 'brady']
+    deck_tokens: ['rws', 'thoth', 'cbd', 'mary', 'brady'],
+    questions: {
+      '1a': "❤️ <strong>Personal:</strong> Click on this card as you think about how you feel right now.",
+      '1b': "❤️ <strong>Personal:</strong> <a href='' class='reloadcard' id='reloadcard_1'>Flip through cards</a> until a card matches how you feel <strong>emotionally</strong> right now.",
+      '2a': "👋 <strong>Social:</strong> Click on this card as you think about how your relationships with others feel right now.",
+      '2b': "👋 <strong>Social:</strong> <a href='' class='reloadcard' id='reloadcard_2'>Flip through cards</a> until a card matches how your <strong>relationships with others</strong> feel right now.",
+      '3a': "🌎 <strong>External:</strong> Click on this card as you think about how the world is feeling right now.",
+      '3b': "🌎 <strong>External:</strong> <a href='' class='reloadcard' id='reloadcard_3'>Flip through cards</a>until a card matches how <strong>the world</strong> feels right now.",
+    }
   }
 
   try {
@@ -38,6 +45,24 @@ router.get('/', function(req, res, next) {
   } catch (e) {
     console.log('Error: '+e);
   }
+
+  // Spread and prompts
+  // a: 
+  spread = req.params['spread']
+  if (spread) {
+    if (spread == 'whats-next') {
+      params['questions'] = {
+      'question': "What's next?",
+      '1a': "<strong>Option 1:</strong>",
+      '1b': "<strong>Option 1:</strong> <a href='' class='reloadcard' id='reloadcard_1'>Flip through cards</a> until you get a card that feels like a viable first option.",
+      '2a': "<strong>Option 2:</strong>",
+      '2b': "<strong>Option 2:</strong> <a href='' class='reloadcard' id='reloadcard_2'>Flip through cards</a> until you get a card that feels like a viable second option.",
+      '3a': "<strong>Option 3:</strong>",
+      '3b': "<strong>Option 3:</strong> <a href='' class='reloadcard' id='reloadcard_3'>Flip through cards</a> until you get a card that feels like a viable third option.",
+    }      
+    }
+  } 
+
 
   res.render('index', params);
 });
